1698e873ace0dc013c368f9c1bd983e79c4f134d
Efim Klaus <nuclearmarmot@yandex.ru>
2025-02-25 11:29:48 +0300

    add allure and surefire plugin in pom

--- src/test/java/AccountNameTest.java
+++ src/test/java/AccountNameTest.java
@@ -1,3 +1,5 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import net.datafaker.Faker;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,12 +10,14 @@ public class AccountNameTest {
 
     public Account account;
 
+    @DisplayName("Проверка минимальной длины имени")
     @Test
     public void minNameLength_ok(){
         Faker faker = new Faker(new Locale("ru"));
         account = new Account(faker.letterify("? ?"));
         Assert.assertTrue(account.checkNameToEmboss());
     }
+    @DisplayName("Проверка максимальной длины имени")
     @Test
     public void maxNameLength_ok(){
         Faker faker = new Faker(new Locale("ru"));
@@ -26,6 +30,7 @@ public class AccountNameTest {
         account = new Account(name);
         Assert.assertTrue(account.checkNameToEmboss());
     }
+    @DisplayName("Стандартная проверка длины имени")
     @Test
     public void averageNameLength_ok() {
         Faker faker = new Faker(new Locale("ru"));
@@ -38,6 +43,7 @@ public class AccountNameTest {
         account = new Account(name);
         Assert.assertTrue(account.checkNameToEmboss());
     }
+    @DisplayName("Проверка на лишние пробелы в середине имени")
     @Test
     public void moreThenOneSpaceInName_fail(){
         Faker faker = new Faker(new Locale("ru"));
@@ -51,6 +57,7 @@ public class AccountNameTest {
         Assert.assertFalse(account.checkNameToEmboss());
 
     }
+    @DisplayName("Проверка пробелов в начале имени")
     @Test
     public void leadSpaceInName_fail(){
         Faker faker = new Faker(new Locale("ru"));
@@ -63,6 +70,7 @@ public class AccountNameTest {
         account = new Account(name);
         Assert.assertFalse(account.checkNameToEmboss());
     }
+    @DisplayName("Проверка пробелов в конце имени")
     @Test
     public void lastSpaceInName_fail(){
         Faker faker = new Faker(new Locale("ru"));
